6515e339fd7a0907d9ab8e70f4a88756a8af8da4
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 10:53:02 +0900

    変数名の付け方を知ろう

--- section3/app.js
+++ section3/app.js
@@ -1,2 +1,12 @@
-var pi = 3.14;
-console.log(pi);
+var color;
+var colorNmae; //キャメルケース, ローワーキャメルケース
+var myColorName;
+var favoriteFood = 20; // bad
+var _color;
+var $name; //上級者向けなの使わあい方がいい
+var name1; //避けた方がいい
+var _nmae1; //避けた方がいい
+var foo = 'small';
+var Foo = 'big';
+console.log(foo);
+console.log(Foo);
--- section3/app.ts
+++ section3/app.ts
@@ -1,4 +1,20 @@
-const pi = 3.14;
-console.log(pi);
+let color;
+let colorNmae; //キャメルケース, ローワーキャメルケース
+let myColorName;
 
-// pi = 3;
+let favoriteFood = 20; // bad
+
+let _color;
+let $name; //上級者向けなの使わあい方がいい
+
+let name1; //避けた方がいい
+let _nmae1; //避けた方がいい
+
+let foo = 'small';
+let Foo = 'big';
+
+console.log(foo);
+console.log(Foo);
+
+//　予約後
+// let const = 'abc';
