e3ccfc6c1ff9f75ad05002aca628119db31814ea
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 16:14:21 +0900

    途中までコミット

---
+++ section6/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function rectanlge(height, width) {
+    return height * width;
+}
+console.log(rectanlge(3, 5));
---
+++ section6/src/app.ts
@@ -0,0 +1,5 @@
+function rectanlge(height:number , width:number) : number {
+    return height * width;
+}
+
+console.log(rectanlge(3,5))
