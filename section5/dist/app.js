5813dd2c8aff385e8d35ae5467c9ba81e1458ced
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 15:18:19 +0900

    演習: 条件分岐 switch

--- section5/dist/app.js
+++ section5/dist/app.js
@@ -1,40 +1,25 @@
 "use strict";
-let num = 3;
-// switch(num) {
-//     case 1:
-//         console.log('Gold');
-//         break;
-//     case 2:
-//         console.log('Silver');
-//         break;
-//     case 3:
-//         console.log('Bronze');
-//         break;
-//     case 4:
-//         console.log('4th');
-//         break;
-//     case 5:
-//         console.log('5th');
-//         break;
-//     default:
-//         console.log('Other');
-//         break;
-// }
-if (num === 1) {
-    console.log('Gold');
-}
-else if (num === 2) {
-    console.log('Silver');
-}
-else if (num === 3) {
-    console.log('Bronze');
-}
-else if (num === 4) {
-    console.log('4th');
-}
-else if (num === 5) {
-    console.log('5th');
-}
-else {
-    console.log('Other');
+// let num = 10;
+let num = Math.floor(Math.random() * 5) + 1;
+//Math.random() * (max - min) + minを表している
+console.log(num);
+switch (num) {
+    case 1:
+        console.log('大吉');
+        break;
+    case 2:
+        console.log('中吉');
+        break;
+    case 3:
+        console.log('小吉');
+        break;
+    case 4:
+        console.log('吉');
+        break;
+    case 5:
+        console.log('凶');
+        break;
+    default:
+        console.log('それ以外');
+        break;
 }
