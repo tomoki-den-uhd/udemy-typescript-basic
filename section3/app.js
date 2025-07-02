ccd85a305660a19003691fd1506b01e21499b253
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 11:11:12 +0900

    基本的な演算子を使おう

--- section3/app.js
+++ section3/app.js
@@ -1,6 +1,26 @@
-var color = 'red';
-console.log(typeof (color));
-var num = 123;
-console.log(typeof (num));
-var b = true;
-console.log(typeof (b));
+//　足し算
+var ans = 1 + 2;
+console.log(ans);
+var i = 20;
+var j = 10;
+var ans1 = i + j;
+console.log(ans1);
+//引き算
+var ans2 = i - j;
+console.log(ans2);
+//掛け算
+var ans3 = i * j;
+console.log(ans3);
+//割り算
+var ans4 = i / j;
+console.log(ans4);
+var x = 3;
+var y = 2;
+var ans5 = x / y;
+console.log(ans5);
+var ans6 = x % y;
+console.log(ans6);
+var ans7 = Math.pow(x, y);
+console.log(ans7);
+var ans8 = x * x;
+console.log(ans8);
--- section3/app.ts
+++ section3/app.ts
@@ -1,9 +1,36 @@
-const color = 'red';
-console.log(typeof(color));
+//　足し算
+const ans = 1+2;
+console.log(ans);
 
-const num = 123;
-console.log(typeof(num));
+const  i = 20;
+const j =10;
+const ans1 = i + j;
+console.log(ans1);
 
-const b = true;
-console.log(typeof(b));
+//引き算
+const ans2 = i - j;
+console.log(ans2);
+
+//掛け算
+const ans3 = i * j;
+console.log(ans3);
+
+//割り算
+const ans4 = i / j;
+console.log(ans4);
+
+const x = 3;
+const y = 2;
+
+const ans5 = x / y ;
+console.log(ans5);
+
+const ans6 = x % y;
+console.log(ans6);
+
+const ans7 = x**y;
+console.log(ans7);
+
+const ans8 = x * x;
+console.log(ans8);
 
