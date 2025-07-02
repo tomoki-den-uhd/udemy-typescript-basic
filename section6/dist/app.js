de2ba3d2e83f61b1ed5a6d22491f3703b6a14064
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 16:42:36 +0900

    アロー関数を使おう

--- section6/dist/app.js
+++ section6/dist/app.js
@@ -1,5 +1,15 @@
 "use strict";
-const getRectangle = function (height, width) {
-    return height * width;
-};
-console.log(getRectangle(3, 5));
+// const getRectangle = function(height: number, width:number) {
+//     return height * width;
+// };
+// console.log(getRectangle(3,5));
+// const getRectangle = (height: number, width: number) => {
+//     return height * width;
+// };
+// console.log(getRectangle(4, 5));
+const getRectangle = (height, width) => height * width;
+console.log(getRectangle(5, 5));
+const putHeight = (height) => console.log(`${height}`);
+putHeight(3);
+const hello = () => console.log('Hello!');
+hello();
--- section6/src/app.ts
+++ section6/src/app.ts
@@ -1,5 +1,20 @@
-const getRectangle = function(height: number, width:number) {
-    return height * width;
-};
+// const getRectangle = function(height: number, width:number) {
+//     return height * width;
+// };
 
-console.log(getRectangle(3,5));
+// console.log(getRectangle(3,5));
+
+// const getRectangle = (height: number, width: number) => {
+//     return height * width;
+// };
+
+// console.log(getRectangle(4, 5));
+
+const getRectangle = (height: number, width: number) => height * width;
+console.log(getRectangle(5,5));
+
+const putHeight = (height: number) => console.log(`${height}`);
+putHeight(3);
+
+const hello = () => console.log('Hello!');
+hello();
