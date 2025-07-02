45d53a06b92dabef486aaa0935ab664c1928d662
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 18:02:30 +0900

    配列の基本を理解しよう app.tsコードエラー修正

--- section7/src/app.ts
+++ section7/src/app.ts
@@ -1,7 +1,7 @@
 // const colors: string[] = ['Red', 'Green', 'Blue'];
 // const colors = [1, 2, 3];
 
-const colors: = ['Red', 'Green', 'Blue'];
+const colors = ['Red', 'Green', 'Blue'];
 console.log(colors);
 
 // const mixedaArr = ['abc', 1, true, false];
