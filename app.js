cfbe0b09d5f4275220edca03c7f8bedfc53cc52a
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 09:53:16 +0900

    プログラムをコメントアウトしよう

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-console.log("Hello World");
-var str = "Hello!";
-console.log(str);
+// console.log('Hello World');
+/*
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+*/ 
--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
-console.log("Hello World");
+// console.log('Hello World');
 
-const str: string = "Hello!";
-console.log(str);
+/*
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+*/
+
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
