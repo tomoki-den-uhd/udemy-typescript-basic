e96776801d13c59f9817d10afbb65fbb09f361d0
TomokiTX57 <tomoki223d@gmail.com>
2025-07-02 18:01:18 +0900

    配列の基本を理解しよう

---
+++ section7/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// const colors: string[] = ['Red', 'Green', 'Blue'];
+// const colors = [1, 2, 3];
+const colors = ['Red', 'Green', 'Blue'];
+console.log(colors);
+// const mixedaArr = ['abc', 1, true, false];
+// console.log(mixedaArr);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+// console.log(colors[3]);
+console.log(colors.length);
+console.log(colors[colors.length - 1]);
+const emptyArr = [];
+console.log(emptyArr);
