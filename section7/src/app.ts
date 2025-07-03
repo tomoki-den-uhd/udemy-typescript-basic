3b20f318b3cd7d44d559f8a33aa10860cff8c0d2
TomokiTX57 <tomoki223d@gmail.com>
2025-07-03 10:19:31 +0900

    タブルを使おう

--- section7/src/app.ts
+++ section7/src/app.ts
@@ -1,73 +1,13 @@
-// オブジェクト(連想配列)
-// いくつかの値をまとめたデータ
-// 1つの変数名でデータをまとめて入れておける
-// プロパティ名と式で一つの要素になる
+//タブルを使う
+//要素のデータ型と個数があらかじめ決まっている配列のようなデータ型
 
+let user: [string, number, string] = ['Lian', 20, 'Tokyo'];
+console.log(user[0]);
+console.log(user[1]);
+console.log(user[2]);
+// console.log(user[3]); //erro 
 
-// name = 'nakamura' 
-// name = プロパティ名(キー)
-// 'nakamura'  = 式(値)
+user = ['Olivia', 21, 'Osaka'];
+// user = [123, 21, 'Osaka']; //error
+// user = ['Olivia', 21]; //error
 
-const user = {
-    name: 'Yuta Nakamura',
-    gender: 'man',
-    age: 21
-};
-
-//ドット記法を使って取り出す
-console.log(user.name);
-console.log(user.gender);
-console.log(user.age);
-
-//ブラケット記法を使って取り出す
-console.log(user['name']);
-console.log(user['gender']);
-console.log(user['age']);
-
-user.age =22;
-console.log(user.age);
-
-
-user['age'] = 23;
-console.log(user['age']);
-
-//プロパティの個数を求める
-console.log(Object.keys(user).length);
-
-//プロパティの一覧を取得
-console.log(Object.keys(user));
-
-//型を明示的に示したい場合
-const user2: {
-    name: string,
-    gender: string,
-    age: number
-} = {
-    name: 'Yuta Nakamura',
-    gender: 'man',
-    age: 20
-};
-// user2.age = '23'; //NG
-// user2.prefecture = 'Tokyo' //NG
-
-console.log(user2.name);
-console.log(user2.gender);
-console.log(user2.age);
-
-
-//インターフェイス宣言でオブジェクトの型をあらかじめ宣言できる
-interface UserObj {
-    name: string,
-    gender: string,
-    age?: number
-}
-
-const user3: UserObj = {
-    name: 'Yuta Nakamura',
-    gender: 'man',
-    // age: 20
-};
-
-console.log(user3.name);
-console.log(user3.gender);
-console.log(user3.age);
