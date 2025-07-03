332adcd91f9167f3d702b4836ec43cd9e0f971ff
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-03 11:37:32 +0900

    クラス宣言をしよう

---
+++ section9/dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+//クラス宣言
+class User {
+    name = '';
+    age = 0;
+}
+const olivia = new User();
+console.log(olivia.name);
+console.log(olivia.age);
+olivia.name = 'Olivia';
+olivia.age = 23;
+console.log(olivia.name);
+console.log(olivia.age);
---
+++ section9/src/app.ts
@@ -0,0 +1,16 @@
+//クラス宣言
+
+class User {
+    name: string = '';
+    age: number = 0;
+}
+
+const olivia = new User();
+console.log(olivia.name);
+console.log(olivia.age);
+olivia.name = 'Olivia';
+olivia.age = 23;
+console.log(olivia.name);
+console.log(olivia.age);
+
+
