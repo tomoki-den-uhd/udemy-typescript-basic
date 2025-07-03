ca29001d2b776de44bad1ad61b0453c78bdb0896
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-03 10:58:07 +0900

    繰り返し処理whileを使おう

--- section8/dist/app.js
+++ section8/dist/app.js
@@ -1,28 +1,16 @@
 "use strict";
-//ループの入れ子
-//二次元配列を取り出すのに使う
-// for (let i = 0; i < 3; i++) {
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`i: ${i}, j: ${j}`);
-//     }
-// }
-//配列の中に配列を入れることw二次元配列という
-const scores = [
-    [100, 99, 98],
-    [90, 89, 88],
-    [80, 79, 78]
-];
-// console.log(scores[0][0]);
-// console.log(scores[0][1]);
-// console.log(scores[0][2]);
-// console.log(scores[1][0]);
-// console.log(scores[1][1]);
-// console.log(scores[1][2]);
-// console.log(scores[2][0]);
-// console.log(scores[2][1]);
-// console.log(scores[2][2]);
-for (let i = 0; i < scores.length; i++) {
-    for (let j = 0; j < scores[i].length; j++) {
-        console.log(scores[i][j]);
+//whileを使おう
+// let i = 1;
+// while (i <=3) {
+//     console.log(i);
+//     i++;
+// };
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    if (j === 5) {
+        break;
     }
+    j++;
 }
+;
--- section8/src/app.ts
+++ section8/src/app.ts
@@ -1,31 +1,16 @@
-//ループの入れ子
-//二次元配列を取り出すのに使う
+//whileを使おう
 
-// for (let i = 0; i < 3; i++) {
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`i: ${i}, j: ${j}`);
-//     }
-// }
+// let i = 1;
+// while (i <=3) {
+//     console.log(i);
+//     i++;
+// };
 
-//配列の中に配列を入れることw二次元配列という
-const scores = [
-    [100, 99, 98],
-    [90, 89, 88],
-    [80, 79, 78]
-];
-
-// console.log(scores[0][0]);
-// console.log(scores[0][1]);
-// console.log(scores[0][2]);
-// console.log(scores[1][0]);
-// console.log(scores[1][1]);
-// console.log(scores[1][2]);
-// console.log(scores[2][0]);
-// console.log(scores[2][1]);
-// console.log(scores[2][2]);
-
-for (let i = 0; i < scores.length; i++) {
-    for (let j = 0; j < scores[i].length; j++) {
-        console.log(scores[i][j]);
+let j = 1;
+while (j <=  10) {
+    console.log(j);
+    if (j === 5){
+        break;
     }
-}
+    j++;
+};
