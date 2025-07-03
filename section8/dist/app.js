100728561e4e001029338ac156c8596feffbb775
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-03 10:51:18 +0900

    ループの入れ子を使おう

--- section8/dist/app.js
+++ section8/dist/app.js
@@ -1,13 +1,28 @@
 "use strict";
-//for文
-// for (let i = 1; i <= 3; i++) {
-//     console.log(i);
-// };
-//配列をfor分で取り出す
-const colors = ['red', 'blue', 'green'];
+//ループの入れ子
+//二次元配列を取り出すのに使う
 // for (let i = 0; i < 3; i++) {
-//     console.log(colors[i]);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`i: ${i}, j: ${j}`);
+//     }
 // }
-for (let i = 0; i < colors.length; i++) {
-    console.log(colors[i]);
+//配列の中に配列を入れることw二次元配列という
+const scores = [
+    [100, 99, 98],
+    [90, 89, 88],
+    [80, 79, 78]
+];
+// console.log(scores[0][0]);
+// console.log(scores[0][1]);
+// console.log(scores[0][2]);
+// console.log(scores[1][0]);
+// console.log(scores[1][1]);
+// console.log(scores[1][2]);
+// console.log(scores[2][0]);
+// console.log(scores[2][1]);
+// console.log(scores[2][2]);
+for (let i = 0; i < scores.length; i++) {
+    for (let j = 0; j < scores[i].length; j++) {
+        console.log(scores[i][j]);
+    }
 }
