a8a50ac4f65e5ff597168cf054b6ca94f52d2576
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-03 10:39:58 +0900

    繰り返し処理forを使おう

---
+++ section8/src/app.ts
@@ -0,0 +1,15 @@
+//for文
+
+// for (let i = 1; i <= 3; i++) {
+//     console.log(i);
+// };
+
+//配列をfor分で取り出す
+const colors = ['red', 'blue', 'green'];
+// for (let i = 0; i < 3; i++) {
+//     console.log(colors[i]);
+// }
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
